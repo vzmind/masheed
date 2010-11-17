7f20d386b7e1f596f8fbe79a46d9051a17b96c7c
Vzmind <vincent.spehner@gmail.com>
2010-11-17 23:03:08 +0100

    Add initial Rails 3 files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Masheed::Application.config.secret_token = '2ae780b10a7af99265c7ef1f765239b0ba2091d8a65478d3a537d4653e7ba9e7a9bb798efb05058ac30e06cafea388015f7175308efe9ebdb9eb0cb6eafe9b2d'
